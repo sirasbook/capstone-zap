e818126609af0efafee94637f05a8d86013504a6
book.siras <book.siras@bitkub.com>
2022-02-07 16:12:32 +0700

    return value JSON respond

--- app/api/scan.py
+++ app/api/scan.py
@@ -4,19 +4,40 @@ from starlette.responses import JSONResponse
 import logging, os, json
 
 router = APIRouter()
+REPORT_FOLDER = '/zap/wrk/app/reports'
+
+def load_json():
+    f = open(REPORT_FOLDER + '/zap-report.json')
+    data = json.load(f)
+    return data
+
+def clear_reports():
+    for file_name in os.listdir(REPORT_FOLDER):
+        file_path = os.path.join(REPORT_FOLDER, file_name)
+        try:
+            os.unlink(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
 @router.post("/basescan", status_code=200)
 def basescan(req_body: Zap_input = Body(...)) -> Response:
     try:
         if req_body:
             url = req_body.url
+            clear_reports()
             os.system(f'/bin/sh -c "app/zap/start-baseline.sh {url}"')
+
+            data = load_json()
+            result = {
+                "alerts": []
+            }
+
+            for obj in data['alerts']:
+                result['alerts'].append(obj)
 
             return JSONResponse(
                 status_code = 200,
-                content = {
-                    "message": "Success",
-                },
+                content = result
             )
         else:
             return JSONResponse(
